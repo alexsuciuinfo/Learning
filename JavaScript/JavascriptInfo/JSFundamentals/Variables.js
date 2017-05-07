3b0dfe75060db39849c8171af47ab1d3abc92b79
alexsuciuinfo <alexsuciuinfo@gmail.com>
2017-05-08 00:13:48 +0300

    Adding variables.js

---
+++ JavaScript/JavascriptInfo/JSFundamentals/Variables.js
@@ -0,0 +1,19 @@
+/*
+Working with variables
+Declare two variables: admin and name.
+Assign the value "John" to name.
+Copy the value from name to admin.
+Show the value of admin using alert (must output “John”).
+*/
+let admin;
+let name = "John";
+admin = name;
+alert(admin);
+
+/*
+Giving the right name
+Create the variable with the name of our planet. How would you name such a variable?
+Create the variable to store the name of the current visitor. How would you name that variable?
+*/
+let ourPlanetName = "Earth";
+let currentUserName = "John";
